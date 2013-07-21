b3f0c692e5d00d8fe0be97f7ac2b810edb09fcd2
Emil Lauridsen <kiljacken@gmail.com>
2013-07-21 16:25:44 +0200

    Remove old code in preparation for rewrite

--- aestuscraft_common/dk/kiljacken/aestuscraft/AestusCraft.java
+++ aestuscraft_common/dk/kiljacken/aestuscraft/AestusCraft.java
@@ -8,78 +8,41 @@
  */
 package dk.kiljacken.aestuscraft;
 
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.network.NetworkMod;
-import cpw.mods.fml.common.network.NetworkRegistry;
-import dk.kiljacken.aestuscraft.addon.AddonLoader;
-import dk.kiljacken.aestuscraft.block.ModBlocks;
-import dk.kiljacken.aestuscraft.core.proxy.CommonProxy;
-import dk.kiljacken.aestuscraft.item.ModItems;
-import dk.kiljacken.aestuscraft.lib.Reference;
-import dk.kiljacken.aestuscraft.lib.StringResources;
-import dk.kiljacken.aestuscraft.network.PacketHandler;
-import dk.kiljacken.aestuscraft.util.ConfigurationHelper;
-import dk.kiljacken.aestuscraft.util.LocalizationHelper;
-import dk.kiljacken.aestuscraft.util.LogHelper;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = Reference.DEPENDENCIES)
-@NetworkMod(clientSideRequired = true, serverSideRequired = true, channels = { Reference.CHANNEL }, packetHandler = PacketHandler.class)
+@Mod(modid = "aec", name = "", version = "")
+//@NetworkMod(clientSideRequired = true, serverSideRequired = true, channels = { "aec" }, packetHandler = PacketHandler.class)
 public class AestusCraft {
-    @Instance(Reference.MOD_ID)
+    @Instance("aec")
     public static AestusCraft instance;
 
-    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
-    public static CommonProxy proxy;
-
-    public static CreativeTabs creativeTab = new CreativeTabs(StringResources.CREATIVE_TAB_NAME) {
-        @Override
-        public ItemStack getIconItemStack() {
-            return new ItemStack(ModBlocks.insulatedFurnace);
-        };
-    };
+//    @SidedProxy(clientSide = "", serverSide = "")
+//    public static CommonProxy proxy;
+//
+//    public static CreativeTabs creativeTab = new CreativeTabs(StringResources.CREATIVE_TAB_NAME) {
+//        @Override
+//        public ItemStack getIconItemStack() {
+//            return new ItemStack(ModBlocks.insulatedFurnace);
+//        };
+//    };
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        LogHelper.init(event.getModLog());
 
-        AddonLoader.init();
-        AddonLoader.initializeAddons();
-
-        LocalizationHelper.init();
-
-        ConfigurationHelper.init(event.getSuggestedConfigurationFile());
-
-        ModBlocks.init();
-        ModItems.init();
-
-        ModBlocks.initRecipes();
-        ModItems.initRecipes();
-
-        AddonLoader.initializeAllBlocks();
-        AddonLoader.initializeAllItems();
-
-        AddonLoader.initializeAllBlockRecipes();
-        AddonLoader.initializeAllItemRecipes();
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        NetworkRegistry.instance().registerGuiHandler(instance, proxy);
 
-        proxy.registerTileEntities();
-        proxy.initializeRendering();
     }
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-        AddonLoader.postInitAll();
+
     }
 }
