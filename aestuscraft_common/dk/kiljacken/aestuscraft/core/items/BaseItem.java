38f9fe9367159b7d26959ffeba84bbe2028dc46e
Emil Lauridsen <kiljacken@gmail.com>
2013-07-22 16:38:24 +0200

    Add base item.

---
+++ aestuscraft_common/dk/kiljacken/aestuscraft/core/items/BaseItem.java
@@ -0,0 +1,17 @@
+/**
+ * AestusCraft
+ * 
+ * BaseItem.java
+ *
+ * @author Kiljacken
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
+package dk.kiljacken.aestuscraft.core.items;
+
+import net.minecraft.item.Item;
+
+public abstract class BaseItem extends Item {
+    public BaseItem(int id) {
+        super(id);
+    }
+}
