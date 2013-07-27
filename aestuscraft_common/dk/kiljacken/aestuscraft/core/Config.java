0e5c1ab8877144bd8bf7f03821fbf85ec2954f35
Emil Lauridsen <kiljacken@gmail.com>
2013-07-27 17:01:59 +0200

    Add conguration options.

--- aestuscraft_common/dk/kiljacken/aestuscraft/core/Config.java
+++ aestuscraft_common/dk/kiljacken/aestuscraft/core/Config.java
@@ -12,6 +12,10 @@ import java.io.File;
 
 import net.minecraftforge.common.Configuration;
 import dk.kiljacken.aestuscraft.AestusCraft;
+import dk.kiljacken.aestuscraft.core.blocks.BlockInfo;
+import dk.kiljacken.aestuscraft.core.blocks.tiles.TileFuelBurner;
+import dk.kiljacken.aestuscraft.core.blocks.tiles.TileInsulatedFurnace;
+import dk.kiljacken.aestuscraft.core.items.ItemInfo;
 
 public class Config {
     public Config(File file) {
@@ -19,9 +23,26 @@ public class Config {
 
         try {
             configuration.load();
+
+            // Block Ids
+            BlockInfo.BLOCK_PRODUCERS_ID = configuration.getBlock(BlockInfo.BLOCK_PRODUCERS_NAME, BlockInfo.BLOCK_PRODUCERS_ID).getInt();
+            BlockInfo.BLOCK_CONSUMERS_ID = configuration.getBlock(BlockInfo.BLOCK_CONSUMERS_NAME, BlockInfo.BLOCK_CONSUMERS_ID).getInt();
+            BlockInfo.BLOCK_HEAT_CONDUCTOR_ID = configuration.getBlock(BlockInfo.BLOCK_HEAT_CONDUCTOR_NAME, BlockInfo.BLOCK_HEAT_CONDUCTOR_ID).getInt();
+
+            // Item Ids
+            ItemInfo.NETWORK_DEBUGGER_ID = configuration.getItem(ItemInfo.NETWORK_DEBUGGER_NAME, ItemInfo.NETWORK_DEBUGGER_ID).getInt();
+
+            // Balancing values
+            {
+                // Fuel burner
+                TileFuelBurner.HEAT_PER_FUEL = (float) configuration.get("balance", "fuelBurner.heatPerFuel", TileFuelBurner.HEAT_PER_FUEL).getDouble(TileFuelBurner.HEAT_PER_FUEL);
+                TileFuelBurner.HEAT_TRANSFER_RATE = (float) configuration.get("balance", "fuelBurner.heatTransferRate", TileFuelBurner.HEAT_TRANSFER_RATE).getDouble(TileFuelBurner.HEAT_TRANSFER_RATE);
+
+                // Insulated furnace
+                TileInsulatedFurnace.HEAT_PER_BURN_TICK = (float) configuration.get("balance", "insulatedFurnace.heatPerBurnTick", TileInsulatedFurnace.HEAT_PER_BURN_TICK).getDouble(TileInsulatedFurnace.HEAT_PER_BURN_TICK);
+            }
         } catch (Exception e) {
             AestusCraft.log.severe("Exception while loading configuration");
-            throw new RuntimeException(e);
         } finally {
             configuration.save();
         }
